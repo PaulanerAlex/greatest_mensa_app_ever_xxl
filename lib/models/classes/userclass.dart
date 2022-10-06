b76ddb2706778b8f27c668b7f27be8dffa741735
PaulanerAlex <paulalex.frick@gmail.com>
2022-10-06 10:40:03 +0200

    restructured file tree - refactoring changes by dart

--- lib/models/classes/userclass.dart
+++
@@ -1,9 +0,0 @@
-class User {
-  int id;
-  String firstname;
-  String lastname;
-  String email;
-  String password;
-
-  User(this.id, this.firstname, this.lastname, this.email, this.password);
-}
--- lib/resources/database.dart
+++ lib/resources/database.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:greatest_mensa_app_ever_xxl/models/resources/firebase_options.dart';
+import 'package:greatest_mensa_app_ever_xxl/resources/firebase_options.dart';
 
 
 // TODO: Add database functions
